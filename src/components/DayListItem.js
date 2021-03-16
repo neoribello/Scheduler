5b2f86575f0051119499520d20d25ad28f8873a0
Neil Oribello <neil.oribello@gmail.com>
2021-03-15 20:44:36 -0700

    storybook list shown/updated

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -10,10 +10,22 @@ export default function DayListItem(props) {
     "day-list__item--full": props.spots === 0
   })
 
+  const formatSpots = (spots) => {
+    if (spots === 0) {
+      return  "no spots remaining"
+    }
+
+    if (spots === 1 ) {
+      return "1 spot remaining"
+    }
+
+    return `${spots} spots remaining`
+  }
+
   return (
     <li className={dayListClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
