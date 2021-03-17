da522ddce81f47230ae3fe833952cb434be23306
Neil Oribello <neil.oribello@gmail.com>
2021-03-16 19:43:34 -0700

    Built the form

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,36 @@
+import React from "react";
+import './styles.scss';
+
+import Button from "../Button";
+import InterviewerList from "../InterviewerList";
+
+export default function Form(props) {
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            value={props.name}
+            type="text"
+            placeholder="Enter Student Name"
+            /*
+              This must be a controlled component
+            */
+          />
+        </form>
+        <InterviewerList 
+          interviewers={props.interviewers} 
+          interviewer={props.interviewer} 
+          onChange={() => {}} 
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button onClick={props.onCancel} danger>Cancel</Button>
+          <Button onClick={props.onSave} confirm>Save</Button>
+        </section>
+      </section>
+    </main>
+  )
+}
--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,11 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 import "index.scss";
 
@@ -149,4 +154,43 @@ storiesOf("InterviewerList", module)
     .add("Appointment", () => <Appointment />)
     .add("Appointment with Time", () => <Appointment time="12pm"/>)
     .add("Header", () => <Header time="12pm" />)
-    .add("Empty", () => <Empty />)
+    .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+    .add("Show", () => 
+    <Show
+      student="Lydia Miller-Jones"
+      interviewer={interviewers[0]}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
+    />)
+    .add("Confirm", () => 
+    <Confirm 
+      message="Delete the appointment?" 
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")}
+    />)
+    .add("Status", () => 
+      <Status message="Deleting" />
+    )
+    .add("Error", () => 
+      <Error 
+        message="Could not delete appointment" 
+        onClose={action("onClose")}
+      />
+    )
+    .add("Edit", () => 
+      <Form 
+        name="Neil"
+        interviewers={interviewers}
+        interviewer={1}
+        onSave={action("onSave")}
+        onCancel={action("onCancel")}
+      />
+    )
+    .add("Create", () => 
+      <Form 
+        interviewers={interviewers}
+        onSave={action("onSave")}
+        onCancel={action("onCancel")}
+      />
+    )
+
