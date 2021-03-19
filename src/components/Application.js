7f768ac39ebb188c48c77e1df445439a8c64281a
neoribello <46947156+neoribello@users.noreply.github.com>
2021-03-19 14:25:16 -0700

    Able to insert data into api and have a transition

--- src/components/Application.js
+++ src/components/Application.js
@@ -37,10 +37,13 @@ export default function Application(props) {
       [id]: appointment
     };
 
-    setState({
-      ...state,
-      appointments
-    });
+    return axios.put(`/api/appointments/${id}`, appointment)
+      .then(() => {
+        setState({
+          ...state,
+          appointments
+        });
+      })
   }
 
   const schedule = appointments.map((appointment) => {
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,15 +1,17 @@
 import React from "react";
+import useVisualMode from "../../hooks/useVisualMode";
 import './styles.scss';
 
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
-import useVisualMode from "../../hooks/useVisualMode";
+import Status from "./Status";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const SAVING = "SAVING";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
@@ -19,20 +21,22 @@ export default function Appointment(props) {
       student: name,
       interviewer
     };
-    
+    transition(SAVING)
+
     props.bookInterview(props.id, interview)
-    transition(SHOW);
-  };
+      .then(() => transition(SHOW))
+  }
 
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === EMPTY &&
+        <Empty onAdd={() => transition(CREATE)} 
+      />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-          
         />
       )}
       {mode === CREATE && (
@@ -42,6 +46,7 @@ export default function Appointment(props) {
           onSave={save}
         />
       )}
+      {mode === SAVING && <Status message={'Saving'}/>}
     </article>
   )
 }
